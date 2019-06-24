c7b9db74d3b4da5a3af10c984fd5128844d683f1
scnu第一帅哥 <376811562@qq.com>
2019-06-24 18:05:43 +0800

    AVLTree

--- 树/chain_avl_binary_tree.h
+++ 树/chain_avl_binary_tree.h
@@ -9,6 +9,8 @@ private:
 	int get_height(BTree<T>*); //获取树的高度
 	int max_int(int, int); //获取int更大值
 	bool insert(T, BTree<T>* &);    //插入指定值节点
+	void rotate_left(BTree<T>* &); //左旋
+	void rotate_right(BTree<T>* &); //右旋
 public:
 	AVLTree() {
 		root = nullptr;
@@ -23,7 +25,54 @@ public:
 	BTree<T>* get_node(T); //获取值的结点
 	BTree<T>* get_root(); //获取头节点
 	bool insert_value(T); //对外插入值函数
+	int get_value_height(T); //获取某个值的高度
+	T search_minimun(BTree<T>*); //查找节点最小值
+	T search_maxmun(BTree<T>*); //查找结点最大值
+	T search_maxmun_root(); //查找最大值
+	T search_minimun_root(); //查找最小值
 };
+template <typename T>
+T AVLTree<T>::search_maxmun(BTree<T>* pnode) {
+	if (pnode->rchild != nullptr)
+		return search_maxmun(pnode->rchild);
+	return pnode->value;
+}
+template <typename T>
+T AVLTree<T>::search_maxmun_root() {
+	return search_maxmun(root);
+}
+template <typename T>
+T AVLTree<T>::search_minimun_root() {
+	return search_minimun(root);
+}
+template <typename T>
+T AVLTree<T>::search_minimun(BTree<T>* pnode) {
+	if (pnode->lchild != nullptr)
+		return search_minimun(pnode->lchild);
+	return pnode->value;
+}
+template <typename T>
+int AVLTree<T>::get_value_height(T value) {
+	return get_height(get_node(value));
+}
+template <typename T>
+void AVLTree<T>::rotate_right(BTree<T>* &p_node) {
+	BTree<T> *left = p_node->lchild;
+	p_node->lchild = left->rchild;
+	left->rchild = p_node;
+	left->height = max_int(get_height(left->lchild), get_height(left->rchild)) + 1;
+	p_node->height = max_int(get_height(p_node->lchild), get_height(p_node->rchild)) + 1;
+	p_node = left;
+}
+template <typename T>
+void AVLTree<T>::rotate_left(BTree<T>* &p_node) {
+	BTree<T> *right = p_node->rchild;
+	p_node->rchild = right->lchild;
+	right->lchild = p_node;
+	right->height = max_int(get_height(right->lchild), get_height(right->rchild)) + 1;
+	p_node->height = max_int(get_height(p_node->lchild), get_height(p_node->rchild)) + 1;
+	p_node = right;
+}
 template <typename T>
 bool AVLTree<T>::insert_value(T value) {
 	return insert(value, root);
@@ -48,9 +97,27 @@ bool AVLTree<T>::insert(T value, BTree<T>* &p_node) {
 	}
 	else if (value > p_node->value) {
 		insert(value, p_node->rchild);
+		if (get_height(p_node->rchild) - get_height(p_node->lchild) == 2) {
+			if (value > p_node->rchild->value) {
+				rotate_left(p_node);
+			}
+			else if (value < p_node->rchild->value) {
+				rotate_right(p_node->rchild);
+				rotate_left(p_node);
+			}
+		}
 	}
 	else if (value < p_node->value) {
 		insert(value, p_node->lchild);
+		if (get_height(p_node->lchild) - get_height(p_node->rchild) == 2) {
+			if (value < p_node->lchild->value) {
+				rotate_right(p_node);
+			}
+			else if (value > p_node->lchild->value)  {
+				rotate_left(p_node->lchild);
+				rotate_right(p_node);
+			}
+		}
 	}
 	p_node->height = max_int(get_height(p_node->lchild), get_height(p_node->rchild)) + 1;
 	return true;
